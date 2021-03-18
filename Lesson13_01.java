1a3b9ccd2231bb21fbe86ee97a254f3a487da2bc
Tanesaka <seedtanesa@outlook.jp>
2021-03-18 16:29:47 +0900

    13

---
+++ Lesson13_01.java
@@ -0,0 +1,17 @@
+public class Lesson13_01{
+    public static void main(String[] args){
+    Student a001 = new Student();
+    a001.name = "sato";
+
+    System.out.println(a001.name);
+    a001.calculateAvg(90,80);
+  }
+}
+
+class Student{
+  String name = "sato";
+
+  public void calculateAvg(int math,int english) {
+    System.out.println((math + english)/2);
+  }
+}
